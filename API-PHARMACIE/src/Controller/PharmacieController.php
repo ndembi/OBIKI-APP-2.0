56ffa5894ebd02ff35a44356ee6b682697687069
ndembi <ndembi_x@etna-alternance.net>
2018-10-18 14:37:30 +0200

    :sparkles: serialisation API Pharmacie

--- API-PHARMACIE/src/Controller/PharmacieController.php
+++ API-PHARMACIE/src/Controller/PharmacieController.php
@@ -5,59 +5,66 @@ namespace App\Controller;
 use App\Entity\Pharmacie;
 use App\Form\PharmacieType;
 use App\Repository\PharmacieRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
  * @Route("/pharmacie")
  */
-class PharmacieController extends AbstractController
+class PharmacieController extends Controller
 {
     /**
      * @Route("/", name="pharmacie_index", methods="GET")
      */
-    public function index(PharmacieRepository $pharmacieRepository): Response
+    public function indexPharmacieAction(PharmacieRepository $pharmacieRepository): Response
     {
-        return $this->render('pharmacie/index.html.twig', ['pharmacies' => $pharmacieRepository->findAll()]);
+         $serializer = $this->get('jms_serializer');
+        $data = $serializer->serialize($pharmacieRepository->findAll(), 'json');
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 
     /**
-     * @Route("/new", name="pharmacie_new", methods="GET|POST")
+     * @Route("/new", name="pharmacie_new", methods="POST")
      */
-    public function new(Request $request): Response
+    public function newPharmacieAction(Request $request): Response
     {
-        $pharmacie = new Pharmacie();
-        $form = $this->createForm(PharmacieType::class, $pharmacie);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($pharmacie);
-            $em->flush();
-
-            return $this->redirectToRoute('pharmacie_index');
+        try {
+        //On recupère les données depuis le formulaire
+        $data = $request->getContent();
+        //On les décodent
+        $pharmacie = $this->get('jms_serializer')->deserialize($data, 'App\Entity\Pharmacie', 'json');
+        //On insert les infos dans la database
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($pharmacie);
+        $em->flush();
+                return new Response('', Response::HTTP_CREATED);
+        } catch (Excepttion $e) {
+            echo 'Exception reçue  : ',  $e->getMessage(), "\n";
         }
 
-        return $this->render('pharmacie/new.html.twig', [
-            'pharmacie' => $pharmacie,
-            'form' => $form->createView(),
-        ]);
     }
 
     /**
      * @Route("/{id}", name="pharmacie_show", methods="GET")
      */
-    public function show(Pharmacie $pharmacie): Response
+    public function showPharmacieAction(Pharmacie $pharmacie): Response
     {
-        return $this->render('pharmacie/show.html.twig', ['pharmacie' => $pharmacie]);
+        $serializer = $this->get('jms_serializer');
+        $data = $serializer->serialize($pharmacie, 'json');
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+
     }
 
     /**
      * @Route("/{id}/edit", name="pharmacie_edit", methods="GET|POST")
      */
-    public function edit(Request $request, Pharmacie $pharmacie): Response
+    public function editPharmacieAction(Request $request, Pharmacie $pharmacie): Response
     {
         $form = $this->createForm(PharmacieType::class, $pharmacie);
         $form->handleRequest($request);
@@ -77,14 +84,12 @@ class PharmacieController extends AbstractController
     /**
      * @Route("/{id}", name="pharmacie_delete", methods="DELETE")
      */
-    public function delete(Request $request, Pharmacie $pharmacie): Response
+    public function deletePharmacieAction(Pharmacie $pharmacie): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$pharmacie->getId(), $request->request->get('_token'))) {
             $em = $this->getDoctrine()->getManager();
             $em->remove($pharmacie);
             $em->flush();
-        }
+            return new Response("delete succès", Response::HTTP_ACCEPTED);
 
-        return $this->redirectToRoute('pharmacie_index');
     }
 }
